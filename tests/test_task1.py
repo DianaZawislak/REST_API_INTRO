a545a5c85db990082af7f800181ebc100b3ad8a1
Diana Zawislak <diana.zawislak@icloud.com>
2022-06-02 23:51:48 -0400

    added test to check if log file exist for new API, fixed pylint errors

--- tests/test_task1.py
+++ tests/test_task1.py
@@ -1,7 +1,12 @@
 """ Write your test for the task here"""
+import os
+from os.path import exists
 
-def test_task(run_program):
-    """Create a unit tests that checks that that your API response log file is created"""
-    # pylint: disable=unused-argument, comparison-with-itself, singleton-comparison
+import app
 
-    assert True == True
+
+def test_logger_for_world_cities_api_response_file(run_program):
+    """Checks for the world_cities_api_response log file"""
+    # pylint: disable=unused-argument
+    debug_log_file_location = os.path.join(app.Config.LOG_DIR, "world_cities_api_response.log")
+    assert exists(debug_log_file_location)
--- tests/test_task2.py
+++ tests/test_task2.py
@@ -5,7 +5,7 @@ import app
 from tests.helpers import search_str_in_file
 
 
-def test_world_cities_api_response(run_program):
+def test_world_cities_api_response_content(run_program):
     # pylint: disable=unused-argument, singleton-comparison, line-too-long
     """checks if the string is found in the Worlds Cities API Response"""
-    assert search_str_in_file(os.path.abspath(os.path.join(app.Config.BASE_DIR, '..', 'logs', 'world_cities_api_response.log')), 'new') == True
+    assert search_str_in_file(os.path.abspath(os.path.join(app.Config.BASE_DIR, '..', 'logs', 'world_cities_api_response.log')), 'Paris') == True
